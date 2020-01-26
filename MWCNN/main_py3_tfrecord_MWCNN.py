b2cb59a1a1b4543a358eec2e640a6ea8adaaa0eb
Lisha <lisha.zhou@tum.de>
2020-01-26 23:48:05 +0100

    add save model

--- MWCNN/main_py3_tfrecord_MWCNN.py
+++ MWCNN/main_py3_tfrecord_MWCNN.py
@@ -8,23 +8,6 @@ from train_MWCNN import *
 from utils_py3_tfrecord_2 import *
 from config import *
 
-#parser = argparse.ArgumentParser(description='')
-#parser.add_argument('--epoch', dest='epoch', type=int, default=50, help='# of epoch')
-#parser.add_argument('--Q', dest='quantization_step', default='10', help='quantization step ')
-#parser.add_argument('--optimizer', dest='optimizer', default='Adam', help='optimizer')
-#parser.add_argument('--batch_size', dest='batch_size', type=int, default=128, help='# images in batch')
-#parser.add_argument('--lr', dest='lr', type=float, default=math.pow(10,-3), help='initial learning rate for adam')
-#parser.add_argument('--patch_size', dest='patch_size', type=int, default=50, help='patch size')
-#parser.add_argument('--use_gpu', dest='use_gpu', type=int, default=1, help='gpu flag, 1 for GPU and 0 for CPU')
-#parser.add_argument('--gpu', dest='num_gpu', type=str, default="0", help='choose which gpu')
-#parser.add_argument('--phase', dest='phase', default='train', help='train or test')
-#parser.add_argument('--checkpoint_dir', dest='ckpt_dir', default='./checkpoint', help='models are saved here')
-#parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
-#parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
-#parser.add_argument('--eval_set', dest='eval_set', default='test', help='dataset for eval in training')
-#parser.add_argument('--test_set', dest='test_set', default='test', help='dataset for testing')
-#args = parser.parse_args()
-
 #weigth decay momentum optimizer
 #L2 regularization
 #tensorboard
@@ -32,13 +15,6 @@ from config import *
 if __name__ == '__main__':
     use_gpu = True
     tf.config.experimental_run_functions_eagerly(True)
-    # check the path of checkpoint, samples and test
-#    if not os.path.exists(args.ckpt_dir):
-#        os.makedirs(args.ckpt_dir)
-#    if not os.path.exists(args.sample_dir):
-#        os.makedirs(args.sample_dir)
-#    if not os.path.exists(args.test_dir):
-#        os.makedirs(args.test_dir)
     if use_gpu:
         print("GPU\n") 
         gpu_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -53,6 +29,5 @@ if __name__ == '__main__':
         
         train_proces = train_MWCNN(batch_size, patch_size, learning_rate)
         train_proces.train_and_checkpoint(train_dataset, epochs, val_dataset)
-
-        model = train_MWCNN(batch_size, patch_size, learning_rate)
-        model.train_and_checkpoint(train_dataset, epochs, val_dataset)
+        train_proces.model.save('./model/model.h5')
+        train_proces.model.save('./model/model', save_format='tf')
--- MWCNN/train_MWCNN.py
+++ MWCNN/train_MWCNN.py
@@ -28,6 +28,10 @@ class train_MWCNN(object):
                 self.__learning_rate, momentum=0.9)
         self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=self.optimizer, net=self.model)
         self.manager = tf.train.CheckpointManager(self.ckpt, './tf_ckpts', max_to_keep=3)
+        self.train_loss_result = []
+        self.train_validation_result = []
+        self.lr = []
+
 
     def loss_fn(self, prediction, groundtruth):
         #inv_converted = wavelet_inverse_conversion(prediction)
@@ -36,46 +40,19 @@ class train_MWCNN(object):
         reg_losses = self.model.losses
         total_loss = tf.add_n([lossRGB] + reg_losses)
         return total_loss
-    
-    def get_adaptive_lr(self, epoch, num_update, num_updates_in_one_epoch):
-        # num_updates in one epoch
-        a = num_update*epoch / num_updates_in_one_epoch
-        if a == 10:
-            self.__learning_rate = self.__learning_rate / 2.0
-        elif a == 20:
-            self.__learning_rate = self.__learning_rate / 10.0
-        elif a == 30:
-            self.__learning_rate = self.__learning_rate / 20.0
-        elif a == 40:
-            self.__learning_rate = self.__learning_rate /100.0
-        else:
-            self.__learning_rate = self.__learning_rate
-        
-        return self.__learning_rate
-        #elif args.optimizer=='SGD':
-            #decay exponentially from 1e-1 to 1e-4 for the 50 epochs.
-        #    for epoch in range (1, epoch):
-        #        lr[iter_epoch*epoch:] = lr[0] * math.pow(10.0,-(3.0/49.0)*epoch)
 
     #for one batch
     @tf.function
-    def train_step(self, images, labels, opt, training):
+    def train_step(self, images, labels, training):
         #converted = wavelet_conversion(images)
         with tf.GradientTape() as tape:
             reconstructed = self.model(images, training = training)
             output = images + reconstructed
-            # test code
-            #plt.imshow(images[1,:,:,:]/255)
-            #plt.show()
-            #plt.imshow(reconstructed[1,:,:,:].numpy()/255)
-            #plt.show()
-            #psnr = imcpsnr(images[1,:,:,:], reconstructed[1,:,:,:])
-            # Compute reconstruction loss
             total_loss = self.loss_fn(output, labels)
         grads = tape.gradient(total_loss, self.model.trainable_weights)
         # Training loop
         # Optimize the model
-        opt.apply_gradients(zip(grads, self.model.trainable_weights))
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
         return total_loss
     
     def evaluate_model(self, val_dataset):
@@ -94,25 +71,26 @@ class train_MWCNN(object):
             print("Restored from {}".format(self.manager.latest_checkpoint))
         else:
             print("Initializing from scratch.")
-        num_updates_in_one_epoch = sum(1 for record in train_dataset)
 
         for epoch in range(1, epochs+1):
+            #keep record of the loss
+            epoch_loss_avg = tf.keras.metrics.Mean()
             print('Start of epoch %d' % (epoch,))
             # Iterate over the batches of the dataset.
-            num_update = 0
             for labels, images in train_dataset:
                 ## main step
-                num_update += 1
-                lr = self.get_adaptive_lr(epoch, num_update, num_updates_in_one_epoch)
-                opt = tf.keras.optimizers.Adam(
-                        lr, name='AdamOptimizer')
-                loss = self.train_step(images, labels, opt, training = True)
+                loss = self.train_step(images, labels, training = True)
+                epoch_loss_avg(loss)
+
                 self.ckpt.step.assign_add(1)
                 if int(self.ckpt.step) % 10 == 0:
                     save_path = self.manager.save()
                     print("Saved checkpoint for step {}: {}".format(int(self.ckpt.step), save_path) + "-- loss {:1.2f}".format(loss.numpy()))
             # evaluate after each epoch
-            self.evaluate_model(val_dataset)
+            self.train_loss_result.append(epoch_loss_avg.result())
+            self.lr.append(self.optimizer.lr)
+            acc = self.evaluate_model(val_dataset)
+            self.train_validation_result.append(acc)
 
 if __name__ == "__main__":
     tf.config.experimental_run_functions_eagerly(True)
