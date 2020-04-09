ec1b62dcc346d8ed26c0d8fb30f2263f9a47adb3
LishaZhouZ <a1995768696.brave@gmial.com>
2020-04-09 13:36:58 +0200

    config to 70 epoch

--- MWCNN/config.py
+++ MWCNN/config.py
@@ -4,7 +4,7 @@ debug_mode = False
 channel = 3
 batch_size = 24
 patch_size = 256
-epochs = 60
+epochs = 70
 record_step = 10
 alpha = 0.01
 decay_lr = np.ones(epochs+1)
@@ -13,7 +13,8 @@ decay_lr[10:20] = alpha/2
 decay_lr[20:30] = alpha/5
 decay_lr[30:40] = alpha/10
 decay_lr[40:50] = alpha/20
-decay_lr[50:epochs+1] = alpha/50
+decay_lr[50:60] = alpha/50
+decay_lr[60:epochs+1] = alpha/100
 
 checkpoint_directory = './tf_ckpts'
 
